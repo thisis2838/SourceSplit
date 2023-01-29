24a8c851dbe10330f85a1bdb955c954ffeaec9f7
thisis2838 <47449032+thisis2838@users.noreply.github.com>
2023-01-29 16:41:58 +0700

    Update CIty17IsFarAway.cs

--- GameSpecific/HL2Mods/CIty17IsFarAway.cs
+++ GameSpecific/HL2Mods/CIty17IsFarAway.cs
@@ -1,61 +1,21 @@
-﻿using LiveSplit.ComponentUtil;
-using System.Diagnostics;
-using LiveSplit.SourceSplit.GameHandling;
-using LiveSplit.SourceSplit.Utilities;
-using System.IO;
+﻿using LiveSplit.SourceSplit.GameHandling;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LiveSplit.SourceSplit.GameSpecific.HL2Mods
 {
     class City17IsFarAway : GameSupport
     {
-        // start: when the view entity index switches from the start camera to the player's
-        // ending: when the view entity index switches from the player's to the end camera
-
-        private int _startCamIndex = -1;
-        private int _endCamIndex = -1;
-
         public City17IsFarAway()
         {
             AddFirstMap("station");
             AddLastMap("finale");
-        }
 
-        protected override void OnSessionStartInternal(GameState state, TimerActions actions)
-        {
-            if (IsFirstMap)
-            {
-                _startCamIndex = state.GameEngine.GetEntIndexByName("start_camera");
-            }
-            else if (IsLastMap)
-            {
-                _endCamIndex = state.GameEngine.GetEntIndexByName("game_end_point_view");
-            }
-        }
-
-        protected override void OnUpdateInternal(GameState state, TimerActions actions)
-        {
-            if (OnceFlag) 
-                return;
-
-            if (IsFirstMap && _startCamIndex != -1)
-            {
-                if (state.PlayerViewEntityIndex.ChangedFromTo(_startCamIndex, 1))
-                {
-                    OnceFlag = true;
-                    Debug.WriteLine("c17 is far away start");
-                    actions.Start();
-                }
-            }
-            else if (IsLastMap && _endCamIndex != -1)
-            {
-                if (state.PlayerViewEntityIndex.ChangedFromTo(1, _endCamIndex))
-                {
-                    OnceFlag = true;
-                    Debug.WriteLine("c17 is far away end");
-                    actions.End();
-                }
-            }
+            WhenCameraSwitchesToPlayer(ActionType.AutoStart, "start_camera");
+            WhenCameraSwitchesFromPlayer(ActionType.AutoEnd, "game_end_point_view");
         }
     }
 }
